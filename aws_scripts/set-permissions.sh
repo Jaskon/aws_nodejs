30afb5785413b16a8055757ff765705a3b3b5129
Eugene Orlov <jaskon97@gmail.com>
2023-03-21 17:07:03 +0100

    Trying to set permissions for codedeploy to work

--- aws_scripts/set-permissions.sh
+++ aws_scripts/set-permissions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -x
+
+# Set ownership for all folders
+chown -R ec2-user:ec2-user /home/ec2-user/app/
 
 # set files to 644 [except *.sh]
 find /home/ec2-user/app/ -type f -not -name "*.sh" -print0 | xargs -0 chmod 0644
