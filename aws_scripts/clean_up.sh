8d54ff32994d3ebf703b6d4ddba4738bb83188de
Eugene Orlov <jaskon97@gmail.com>
2023-03-21 16:35:13 +0100

    Fix aws scripts for codedeploy

--- aws_scripts/clean_up.sh
+++ aws_scripts/clean_up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+set -x
 
 
 rm -rf /home/ec2-user/app/node_modules
--- aws_scripts/start_server.sh
+++ aws_scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+set -x
 
 
 pm2 start /home/ec2-user/app/src/index.js
