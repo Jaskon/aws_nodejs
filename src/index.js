3a219cfabb3642a26cd34066878b3367c8a02f1f
Eugene <jaskon97@gmail.com>
2023-03-22 23:32:02 +0100

    Add and serve index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World v3!');
-});
+
+app.use(express.static(path.join(__dirname, '../frontend-app/public')));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
