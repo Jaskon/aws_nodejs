6513eade593b91a2cff1184150599ca799f56c55
Eugene <jaskon97@gmail.com>
2023-03-24 00:46:55 +0100

    Add frontend-app created by next.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static(path.join(__dirname, '../frontend-app/public')));
+app.use(express.static(path.join(__dirname, '../frontend-app/dist')));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
