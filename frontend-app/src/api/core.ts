2eb239358a753389d41feb410de1875e59862451
Eugene <jaskon97@gmail.com>
2023-03-25 02:41:06 +0100

    Fix BASE_URL variable

--- frontend-app/src/api/core.ts
+++ frontend-app/src/api/core.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
 import { User } from '../model/User';
 
-const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = '';
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = '';
 
 export const getUsers = (): Promise<AxiosResponse<Array<User>>> => {
     return axios.get(BASE_URL + '/users');
