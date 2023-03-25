f2cd5e6359100ef461005c8cccb32fdb0ee58095
Eugene <jaskon97@gmail.com>
2023-03-25 02:37:43 +0100

    Push also 'build' folder
    Later will generate this with CD

---
+++ frontend-app/src/api/core.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosResponse } from 'axios';
+import { User } from '../model/User';
+
+// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = '';
+
+export const getUsers = (): Promise<AxiosResponse<Array<User>>> => {
+    return axios.get(BASE_URL + '/users');
+};
