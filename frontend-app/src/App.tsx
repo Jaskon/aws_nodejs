7122420dafed7db16175f18ff2953e56d31bc68f
Eugene <jaskon97@gmail.com>
2023-03-25 02:34:14 +0100

    Replace frontend-app
    Use react-create-app to generate one, using typescript template.
    Also trying to pass a request to the server and get a result.

---
+++ frontend-app/src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { getUsers } from './api/core';
+import { User } from './model/User';
+
+function App() {
+  const [ users, setUsers ] = useState<Array<User>>([]);
+
+  useEffect(() => {
+    getUsers().then(res => setUsers(res.data));
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          Users list (async):
+        </div>
+        <div className={'m-bt-2'}>
+          { Object.values(users).map((user) =>
+            <div key={user.id}>
+              { user.name } { user.surname }
+            </div>
+          ) }
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const express = require('express');
 const app = express();
 const port = 3000;
 
 
-app.use(express.static(path.join(__dirname, '../frontend-app/dist')));
+// CORS
+app.use(cors({ origin: 'http://localhost:3001' }));
+
+app.get('/users', (req, res, next) => {
+    res.json([
+        { id: '1', name: 'John', surname: 'Cena'},
+        { id: '2', name: 'Michael', surname: 'Jackson'},
+        { id: '3', name: 'Alan', surname: 'Damn'},
+    ]);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '../frontend-app/build')));
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App listening on http://localhost:${port}`);
 });
