42742ba47333c886ebde8900e5b8f3ef47841dcf
Eugene <jaskon97@gmail.com>
2023-03-27 02:43:53 +0200

    Move Users to a separate component

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,33 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
-import { getUsers } from './api/core';
-import { User } from './model/User';
 import Chat from './components/chat/Chat';
+import Users from './components/users/Users';
 
 function App() {
-  const [ users, setUsers ] = useState<Array<User>>([]);
-
-  useEffect(() => {
-    getUsers().then(res => setUsers(res.data));
-  }, []);
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <img src={logo} className={styles.logo} alt="logo" />
 
-        <div>
-          Users list (async):
-        </div>
-        <div className={'m-bt-2'}>
-          { Object.values(users).map((user) =>
-            <div key={user.id}>
-              { user.name } { user.surname }
-            </div>
-          ) }
-        </div>
-
+        <Users />
         <Chat />
       </div>
     </div>
---
+++ frontend-app/src/components/users/Users.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { getUsers } from '../../api/core';
+import { User } from '../../model/User';
+
+function Users() {
+    const [ users, setUsers ] = useState<Array<User>>([]);
+
+    useEffect(() => {
+        getUsers().then(res => setUsers(res.data));
+    }, []);
+
+    return <>
+        <div>
+            Users list (testing async):
+        </div>
+        <div className={'m-bt-2'}>
+            { Object.values(users).map((user) =>
+                <div key={user.id}>
+                    { user.name } { user.surname }
+                </div>
+            ) }
+        </div>
+    </>;
+}
+
+export default Users;
