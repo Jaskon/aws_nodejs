2f0f0d80e5555a0dcce05ba2d0e74348f2e25ac9
Eugene Orlov <jaskon97@gmail.com>
2023-04-19 02:26:36 +0600

    Check after_install execution time

--- aws_scripts/install_dependencies.sh
+++ aws_scripts/install_dependencies.sh
@@ -3,5 +3,5 @@ source /home/ec2-user/.bash_profile
 set -x
 
 
-#npm install --prefix /home/ec2-user/app
+npm install --prefix /home/ec2-user/app
 #npm install --prefix /home/ec2-user/app/frontend-app
